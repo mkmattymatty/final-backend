636dc826be44bd6b7d304b84f4a43fdedf095b98
Mathias Mwaro <mattymatty9372@gmail.com>
2025-11-13 17:06:09 +0300

    Update MongoDB connection URI variable

--- seed.js
+++ seed.js
@@ -63,7 +63,7 @@ const departments = [
 
 const seedDatabase = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('Connected to MongoDB');
 
     await Department.deleteMany({});
